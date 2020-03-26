351923a4d4d07de28197b92207ce1cd70ac73bfc
Sheikh Haris Zahid <sheikh.haris.zahid@hotmail.com>
2020-03-27 00:34:36 +0500

    Handle the "Zero Frequency Problem"

--- nbc.py
+++ nbc.py
@@ -1,5 +1,23 @@
 import pandas as pd
 
+class LIST:
+    def __init__(self):
+        self.lst = []
+        super().__init__()
+    def addToList(self, num):
+        if num in self.lst:
+            return
+        # Sorted Insertion List
+        size = len(self.lst)   
+        for i in range(0, size):
+            prev = i
+            if self.lst[i] > num:
+                self.lst.insert(prev, num)
+                return
+        self.lst.append(num)
+        return
+    def getLength(self):
+        return len(self.lst)
 
 class message(object):
     def __init__(self):
@@ -37,10 +55,24 @@ def findLikelihood(data, cls, vars):
     # Finding P(x=x,y=y,z=z | output = cl )
     for col in data.columns:
         if col == 'o': break
-        result *= len(data.loc[(data[col] == vars[index])
-                                  & (data['o'] == cls)]) / total
+        ans = len(data.loc[(data[col] == vars[index])
+                           & (data['o'] == cls)]) / total
+        # if prior become 0
+        if ans == 0:
+            # geting t which is number of integers
+            # and we need it for p = 1/t
+            slist = LIST()
+            for i in range(0, len(data)):
+                slist.addToList(int(data['x'][i]))
+                slist.addToList(int(data['y'][i]))
+                slist.addToList(int(data['z'][i]))
+            # (a + m*p)/(b + m) <= m-estimation formula
+            p = (1/slist.getLength())
+            ans = (0 + (len(data)*p))/(total+len(data))
+        result *= ans
         index += 1
-    result *= total/len(data)
+    result *= (total/len(data))
+    
     return result
 
 def predictClass(x, y, z):
@@ -53,6 +85,7 @@ def predictClass(x, y, z):
 
     # read dataset
     data = readDataset()
+        
 
     # get Classes
     classes = readClasses()
